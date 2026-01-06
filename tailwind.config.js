23ccd81a594e3bc083003407d4e5523d0e5bb1e8
Arthur Bender <arthurllbender@gmail.com>
2026-01-06 17:18:07 -0300

    Feat: define initial tailwind colors

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#004fde",
+        secondary: "#007cf8",
+        dark: "#0a06a5",
+        accent: "##00a2f6"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
