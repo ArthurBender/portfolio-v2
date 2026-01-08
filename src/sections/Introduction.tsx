e44d614e9ce7457bc68c994a959b1fe08e2947c8
Arthur Bender <arthurllbender@gmail.com>
2026-01-08 11:28:54 -0300

    Fix: introduction text dark theme

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -20,7 +20,7 @@ const Introduction = () => {
           </div>
         </div>
 
-        <div className="flex-1 bg-gray-300 p-6 overflow-hidden rounded-xl opacity-70">
+        <div className="flex-1 bg-background text-white border border-white shadow-lg p-6 overflow-hidden rounded-xl opacity-70">
           I'm a full-stack developer with more than 8 years of experience in the field, passionate about technology and development. I really enjoy learning and I am a lively and positive person, facilitating teamwork.<br /><br />
           I have solid knowledge of Ruby on Rails, having worked with the framework for over 6 years. Recently, I have been working on projects using React, improving my Frontend skills. I have also used technologies such as Python, Android, PHP, and Docker in various projects.<br /><br />
           My goal is to continue growing professionally, learning new languages and technologies, or deepening my expertise in those I already master, contributing with efficient and innovative solutions to the projects I work on.
