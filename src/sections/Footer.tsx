2943dfdd4dd2d53b615007994cccbab2286faa52
Arthur Bender <arthurllbender@gmail.com>
2026-01-15 01:50:57 -0300

    Feat: add Footer en translations

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next";
+
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <footer className="bg-surface text-text text-sm">
       <div className="flex flex-col md:flex-row justify-between px-4 py-2">
-        <span>Designed and built by Arthur Bender</span>
-        <span>© {new Date().getFullYear()}. All rights reserved.</span>
+        <span>{t("footer.text")}</span>
+        <span>© {new Date().getFullYear()}. {t("footer.copyright")}</span>
       </div>
     </footer>
   )
